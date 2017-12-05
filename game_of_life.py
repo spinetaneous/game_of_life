522f5f8f0df788891680184a7223f3f3029c94ec
Irene <ireneho.imyh@gmail.com>
2017-12-05 14:06:09 -0800

    some comments lol

--- game_of_life.py
+++ game_of_life.py
@@ -14,8 +14,24 @@ gen2 = [0 for x in range(len(gen1))]
 def make_gen(gen1, rows, cols):
     """
     makes a new generation
+	this function assumes that the cells outside the border (off the grid)
+	are dead i.e. 0s
     """
-
+	for i in range(rows):
+		for j in range(cols):
+			counter = 0 # keeps track of how many surrounding cells are alive
+			# check top
+			
+			# check left
+			
+			# check right
+			
+			# check bottom
+			
+			# determine if cell should be alive or dead
+			
+			pass
+		
 def print_gen(gen, rows, cols):
     """
     prints a new generation
